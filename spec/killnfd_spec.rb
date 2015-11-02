fef03fbf8d1b6554c61ec3bbbcac503d34137c49
osyoyu <osyoyu@osyoyu.com>
2015-11-02 15:33:58 +0900

    Write some tests for String#to_nfc, String#to_nfd and String#is_nfd?

--- spec/killnfd_spec.rb
+++ spec/killnfd_spec.rb
@@ -1,11 +1,25 @@
 require 'spec_helper'
 
-describe Killnfd do
+describe KillNFD do
   it 'has a version number' do
-    expect(Killnfd::VERSION).not_to be nil
+    expect(KillNFD::VERSION).not_to be nil
+  end
+end
+
+describe String do
+  it 'correctly converts NFD to NFC' do
+    expect("\u304B\u3099\u304D\u3099\u304F\u3099\u3051\u3099\u3053\u3099".to_nfc).to eq("\u304C\u304E\u3050\u3052\u3054")
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'correctly converts NFC to NFD' do
+    expect("\u304C\u304E\u3050\u3052\u3054".to_nfd).to eq("\u304B\u3099\u304D\u3099\u304F\u3099\u3051\u3099\u3053\u3099")
+  end
+
+  it 'says a NFC string is not NFD' do
+    expect("\u304C\u304E\u3050\u3052\u3054".is_nfd?).not_to be true
+  end
+
+  it 'says a NFD string is NFD' do
+    expect("\u304B\u3099\u304D\u3099\u304F\u3099\u3051\u3099\u3053\u3099".is_nfd?).to be true
   end
 end
